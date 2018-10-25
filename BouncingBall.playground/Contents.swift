24b00b47027e1fd0364c9f2b6436f95540265436
Jessica Joseph <jessjeanjo@gmail.com>
2018-10-25 14:35:50 -0400

    first go at getting the animation working #buggy

--- BouncingBall.playground/Contents.swift
+++ BouncingBall.playground/Contents.swift
@@ -22,6 +22,46 @@ class MyViewController : UIViewController {
         let imageView = UIImageView(frame: CGRect(x: 155 , y: 200, width: 75, height: 75))
         imageView.image = image
         
+        // animation
+        let translation: CAKeyframeAnimation = CAKeyframeAnimation(keyPath: "transform.translation.y")
+        let values = [-200, 20, 0]
+        translation.values = values
+        
+        let times: [NSNumber] = [0.0, 0.85, 1]
+        translation.keyTimes = times
+        translation.timingFunctions = [CAMediaTimingFunction(name: .easeIn)]
+        translation.autoreverses = true
+        translation.duration = 1.0
+        translation.repeatCount = .infinity
+        
+        
+        let scaleXAnimation: CAKeyframeAnimation = CAKeyframeAnimation(keyPath: "transform.scale.x")
+        let scaleXalues = [0.75, 0.75, 1]
+        scaleXAnimation.values = scaleXalues
+        
+        scaleXAnimation.keyTimes = times
+        scaleXAnimation.timingFunctions = [CAMediaTimingFunction(name: .linear)]
+        scaleXAnimation.autoreverses = true
+        scaleXAnimation.duration = 1.0
+        scaleXAnimation.repeatCount = .infinity
+        
+        
+        
+        let scaleYAnimation: CAKeyframeAnimation = CAKeyframeAnimation(keyPath: "transform.scale.y")
+        let scaleYValues = [0.75, 1, 0.85]
+        translation.values = scaleYValues
+        
+        let scaleYTimes: [NSNumber] = [0.1, 0.5, 1]
+        scaleYAnimation.keyTimes = scaleYTimes
+        scaleYAnimation.timingFunctions = [CAMediaTimingFunction(name: .linear)]
+        scaleYAnimation.autoreverses = true
+        scaleYAnimation.duration = 1.0
+        scaleYAnimation.repeatCount = .infinity
+        
+        imageView.layer.add(scaleXAnimation, forKey: "scaleX")
+        imageView.layer.add(scaleYAnimation, forKey: "scaleY")
+        imageView.layer.add(translation, forKey: "trans")
+        
         view.addSubview(button)
         view.addSubview(imageView)
         self.view = view
