5aac06f2b7b092271dd360628091e8540a2ca514
Jessica Joseph <jessjeanjo@gmail.com>
2018-10-25 13:20:18 -0400

    add ball image and finish setup for bouncing animation

--- BouncingBall.playground/Contents.swift
+++ BouncingBall.playground/Contents.swift
@@ -10,10 +10,20 @@ class MyViewController : UIViewController {
 
         let button = UIButton(frame: CGRect(x: 110, y: 120, width: 150, height: 50))
         button.backgroundColor = .magenta
-        button.setTitle("Drop the ball", for: .normal)
-        button.setTitleColor(.white , for: .normal)
+        
+        let buttonText = NSAttributedString(string: "Drop, ball!!", attributes: [.font: UIFont(name: "Helvetica-Bold", size: 15.0), .foregroundColor: UIColor.white])
+        button.setAttributedTitle(buttonText, for: .normal)
+        
+        button.layer.cornerRadius = 25
+        button.layer.borderWidth = 3
+        button.layer.borderColor = UIColor.white.cgColor
+        
+        let image = UIImage(named: "beach-ball")
+        let imageView = UIImageView(frame: CGRect(x: 155 , y: 200, width: 75, height: 75))
+        imageView.image = image
         
         view.addSubview(button)
+        view.addSubview(imageView)
         self.view = view
     }
 }
