7e36950332edcc4c0c95f71d1cd2a9116ec53274
Jessica Joseph <jessjeanjo@gmail.com>
2018-10-26 15:52:29 -0400

    we have balls bouncing, no physics into play, but animation is working

--- BouncingBall.playground/Contents.swift
+++ BouncingBall.playground/Contents.swift
@@ -30,43 +30,13 @@ class MyViewController : UIViewController {
         imageView.image = image
         
         // animation
-        let translation: CAKeyframeAnimation = CAKeyframeAnimation(keyPath: "transform.translation.y")
-        let values = [-200, 20, 0]
-        translation.values = values
-        
-        let times: [NSNumber] = [0.0, 0.85, 1]
-        translation.keyTimes = times
-        translation.timingFunctions = [CAMediaTimingFunction(name: .easeIn)]
-        translation.autoreverses = true
-        translation.duration = 1.0
-        translation.repeatCount = .infinity
-        
-        
-        let scaleXAnimation: CAKeyframeAnimation = CAKeyframeAnimation(keyPath: "transform.scale.x")
-        let scaleXalues = [0.75, 0.75, 1]
-        scaleXAnimation.values = scaleXalues
-        
-        scaleXAnimation.keyTimes = times
-        scaleXAnimation.timingFunctions = [CAMediaTimingFunction(name: .linear)]
-        scaleXAnimation.autoreverses = true
-        scaleXAnimation.duration = 1.0
-        scaleXAnimation.repeatCount = .infinity
-        
-        let scaleYAnimation: CAKeyframeAnimation = CAKeyframeAnimation(keyPath: "transform.scale.y")
-        let scaleYValues = [0.75, 1, 0.85]
-        translation.values = scaleYValues
-        
-        let scaleYTimes: [NSNumber] = [0.1, 0.5, 1]
-        scaleYAnimation.keyTimes = scaleYTimes
-        scaleYAnimation.timingFunctions = [CAMediaTimingFunction(name: .linear)]
-        scaleYAnimation.autoreverses = true
-        scaleYAnimation.duration = 1.0
-        scaleYAnimation.repeatCount = .infinity
-        
-        imageView.layer.add(scaleXAnimation, forKey: "scaleX")
-        imageView.layer.add(scaleYAnimation, forKey: "scaleY")
-        imageView.layer.add(translation, forKey: "trans")
-        
+        UIView.animate(withDuration: 1, animations: {
+            imageView.frame.origin.y += self.view.frame.size.height - imageView.frame.size.height - imageView.frame.origin.y
+        }) { (_) in
+            UIView.animateKeyframes(withDuration: 1, delay: 0.25, options: [.autoreverse, .repeat], animations: {
+                imageView.frame.origin.y -= 300
+            }, completion: nil)
+        }
         view.addSubview(imageView)
     }
 }
